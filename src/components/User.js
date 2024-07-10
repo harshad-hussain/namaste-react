4e4ee9379f0747cd383a5d8add8cc4cb7cd28fda
Harshad-John22 <harshad.kanini@gmail.com>
2024-07-10 07:55:43 -0500

    learned how to use useState hook in class based components and as well as how the useState hook works behind the scenes .

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 const User = (props) => {
+
+  const[count1,setCount] = useState(0);
     return(
-        <div>
+        <div className="user-card">
+          <h2>Count:{count1}</h2>
+          <button  onClick={()=>{
+             setCount(count1+1);
+          }}> click me </button>
       <h2>Name :{props.name}</h2>
       <h2>location :{props.location}</h2>
       </div>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,12 +4,26 @@ class UserClass extends React.Component{
  
     constructor(props){
         super(props);
+
+        this.state ={
+          count1:0,
+          count2:1,
+        };
     }
+
+   
     
     render(){
         const{name,location} = this.props;
+        const{count1,count2} = this.state;
         return(
-            <div>
+            <div className="user-card">
+                <h3>Count:{count1}</h3>
+                <button onClick={()=>{
+                    this.setState({
+                        count1:this.state.count1+1,
+                    })
+                }}>Click me</button>
                 <h2>Name:{name}</h2>
                 <h2>location:{location}</h2>
             </div>
